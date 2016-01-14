9d493e479233ffb9c8a79ef4c191cf721bc73deb
sinaxgy <191127340.qq.com>
2016-01-14 22:42:26 +0800

    Initial Commit

---
+++ AutoLifeTests/AutoLifeTests.swift
@@ -0,0 +1,36 @@
+//
+//  AutoLifeTests.swift
+//  AutoLifeTests
+//
+//  Created by 徐成 on 16/1/14.
+//  Copyright © 2016年 徐成. All rights reserved.
+//
+
+import XCTest
+@testable import AutoLife
+
+class AutoLifeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
